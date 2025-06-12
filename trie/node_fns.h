3b353d5e297deca380c111c8a4bb7c02cd6d9e32
Tbzz83 <Tbzz83@gmail.com>
2025-06-11 23:56:47 -0400

    Renaming and comments for clarity

--- trie/node_fns.h
+++ trie/node_fns.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "node.h"
 
-bool char_in_children(Node **children, int char_d, char c) {
-  Node **cur = children + char_d;
+bool char_in_children(Node **children, int child_decimal, char c) {
+  Node **cur = children + child_decimal;
   // *cur is the pointer to the child Node
   if (*cur == NULL){
     printf("char: %c is NOT a child.\n", c);
@@ -18,11 +18,11 @@ bool char_in_children(Node **children, int char_d, char c) {
 bool starts_with(Node *head, char *string, size_t string_len) {
   Node *cur = head;
   for (int i=0; i < string_len; i++) {
-    int char_d = string[i]-'a';
-    if (!char_in_children(cur->children, char_d, string[i])) {
+    int child_decimal = string[i]-'a';
+    if (!char_in_children(cur->children, child_decimal, string[i])) {
       return false;
     }
-    cur = *(cur->children + char_d); 
+    cur = *(cur->children + child_decimal); 
   }
   return true;
 }
@@ -36,12 +36,15 @@ bool search(Node *head, char *string, size_t string_len) {
   Node *cur = head;
 
   for (int i=0; i < string_len; i++) {
-    int char_d = string[i]-'a';
+    int child_decimal = string[i]-'a';
     
     if (string[i] == '.') {
       // '.' is a wildcard character
       for (int j=0; j <= 26; j++) {
         if (cur->children + j) {
+          // &string[i+1] is the remaining string needed to search
+          // string_len value is updated by subtracting the difference of the indices 
+          // for characters already searched
           if (search(*(cur->children + j), &string[i+1], string_len - (i+1))) {
             return true;
           }
@@ -52,8 +55,8 @@ bool search(Node *head, char *string, size_t string_len) {
       // for loop, then we must return false. In this event, there are too many '.' characters
       // at the end of the word
       return false;
-    } else if (char_in_children(cur->children, char_d, string[i])) {
-      cur = *(cur->children + char_d);
+    } else if (char_in_children(cur->children, child_decimal, string[i])) {
+      cur = *(cur->children + child_decimal);
     } else {
       return false;
     }
@@ -70,13 +73,13 @@ void insert(Node *head, char *string, size_t string_len) {
   printf("\ninserting '%s'...\n", string);
   for (int i=0; i < string_len; i++) {
     // Taking the decimal value of 'char' - 'a' returns indexes in the size of an array of 26 characters
-    int char_d = string[i]-'a'; 
+    int child_decimal = string[i]-'a'; 
     
-    if (char_in_children(cur->children, char_d, string[i])) {// if character is child
-      cur = *(cur->children + char_d);
-    } else { // else create new node, add it as a child for cur, cur = (*(cur->children)) + char_d
+    if (char_in_children(cur->children, child_decimal, string[i])) {// if character is child
+      cur = *(cur->children + child_decimal);
+    } else { // else create new node, add it as a child for cur, cur = (*(cur->children)) + child_decimal
       Node *new_node = create_node(); 
-      *(cur->children + char_d) = new_node;
+      *(cur->children + child_decimal) = new_node;
       new_node->c = string[i];
       cur = new_node;
     }
