8d0fd758263b00579a918b73c831cb5e88c9c78e
Tbzz83 <Tbzz83@gmail.com>
2025-10-13 15:38:29 -0400

    starting clone graph problem

---
+++ graphs/clone_graph/main.py
@@ -0,0 +1,39 @@
+#Given a node in a connected undirected graph, return a deep copy of the graph.
+#
+#Each node in the graph contains an integer value and a list of its neighbors.
+#
+#class Node {
+#    public int val;
+#    public List<Node> neighbors;
+#}
+#
+#The graph is shown in the test cases as an adjacency list. An adjacency list is a mapping of nodes to lists, used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+#
+#For simplicity, nodes values are numbered from 1 to n, where n is the total number of nodes in the graph. The index of each node within the adjacency list is the same as the node's value (1-indexed).
+#
+#The input node will always be the first node in the graph and have 1 as the value.
+#
+# In some way, recurse through the original graph. 
+# Initialize our clone to have a head node with a value of 1
+# 1. Loop through cur_nodes children, clone each child. 
+# 2. append each child_clone to cur_clone children
+# 3. set cur_node to each child, repeat 1->3.
+#
+# Could be done both recursively or iteratively. Let's try recursive DFS
+
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val = 0, neighbors: list[Optional["Node"]] = []) -> None:
+        self.val = val
+        self.neighbors: list[Node] = neighbors if neighbors else []
+
+def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
