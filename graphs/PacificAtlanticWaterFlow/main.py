dfc70987fdbef76942205d39fccaa1204d38a538
Tbzz83 <Tbzz83@gmail.com>
2025-10-22 15:10:43 -0400

    Pacific Atlantic Water Flow passing... tough one

--- graphs/PacificAtlanticWaterFlow/main.py
+++ graphs/PacificAtlanticWaterFlow/main.py
@@ -1,72 +1,46 @@
+#You are given a rectangular island heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+#
+#The islands borders the Pacific Ocean from the top and left sides, and borders the Atlantic Ocean from the bottom and right sides.
+#
+#Water can flow in four directions (up, down, left, or right) from a cell to a neighboring cell with height equal or lower. Water can also flow into the ocean from cells adjacent to the ocean.
+#
+#Find all cells where water can flow from that cell to both the Pacific and Atlantic oceans. Return it as a 2D list where each element is a list [r, c] representing the row and column of the cell. You may return the answer in any order.
+# 
+# We are going to do two 
 
 
-def pacificAtlantic(heights: list[list[int]]) -> list[list[int]]:
-
+def pacificAtlantic(heights: list[list[int]]):
     ROWS, COLS = len(heights), len(heights[0])
-    big_res: list[list[int]] = []
-    seen: dict[tuple[int, int], list[int]] = {}
+    pac, atl = set(), set()
+    res = []
 
+    def dfs(r: int,c:int ,visit: set[tuple[int,int]], prev_height: int):
+        if ((r,c) in visit or 
+            r not in range(ROWS) or
+            c not in range(COLS) or 
+            heights[r][c] < prev_height):
+            return
 
-    # ATLANTIC = 1
-    # PACIFIC = 2
-    def in_water_check(r, c) -> int:
-        if r >= ROWS:
-            return 2
-        elif r < 0:
-            return 1
-        elif c >= COLS:
-            return 2
-        # if c < COLS
-        return 1
+        visit.add((r,c))
 
-    def dfs(r: int, c: int, parent_val: int) -> list[int]:
-        res = []
-        if (r not in range(ROWS)) or (c not in range(COLS)):
-            res.append(in_water_check(r,c))
-            return res
+        dfs(r+1,c,visit,heights[r][c])
+        dfs(r-1,c,visit,heights[r][c])
+        dfs(r,c+1,visit,heights[r][c])
+        dfs(r,c-1,visit,heights[r][c])
 
-        val = heights[r][c]
-        if parent_val < val:
-            return []
-        elif (r,c) in seen:
-            return seen[(r,c)]
+    for c in range(COLS):
+        dfs(0, c, pac, heights[0][c])
+        dfs(ROWS-1, c, atl, heights[ROWS-1][c])
 
-        def valid(l: list[int]):
-            for num in l:
-                if num == 1 and 1 not in res:
-                    res.append(1)
-                elif num == 2 and 2 not in res:
-                    res.append(2)
+    for r in range(ROWS):
+        dfs(r, 0, pac, heights[r][0])
+        dfs(r, COLS-1, atl, heights[r][COLS-1])
 
-        seen[(r,c)] = []
-
-        left = dfs(r, c-1, val)
-        valid(left)
-        seen[(r,c)] = res
-
-        right = dfs(r, c+1, val)
-        valid(right)
-        seen[(r,c)] = res
-
-        up = dfs(r-1, c, val)
-        valid(up)
-        seen[(r,c)] = res
-
-        down = dfs(r+1, c, val)
-        valid(down)
-        seen[(r,c)] = res
-
-        if len(res) == 2:
-            big_res.append([r,c])
-        print(r,c,res)
-
-        return res
-    
     for r in range(ROWS):
         for c in range(COLS):
-            dfs(r,c,heights[r][c])
-
-    return(big_res)
+            if (r,c) in pac and (r,c) in atl:
+                res.append([r,c])
+    return res
 
 def main():
 #    heights = [
@@ -75,7 +49,6 @@ def main():
 #  [6,3,5,3,6]
 #]
     heights=[[1,1],[1,1],[1,1]]
-    heights=[[10,10,10],[10,1,10],[10,10,10]]
     print(pacificAtlantic(heights))
 
 
