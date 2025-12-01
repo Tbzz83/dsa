40713514e4b8ad9d757a17a6b2c4acb701168750
tbzz83 <tbzz83@gmail.com>
2025-12-01 08:09:09 -0500

    Shout out neetcode bc this question ws fkd

--- dp/2d/burst_balloons/main.py
+++ dp/2d/burst_balloons/main.py
@@ -14,18 +14,32 @@
 # nums = [4,2,3,7] --> [4,3,7] --> [4,7] --> [7] --> []
 # coins =  4*2*3    +   4*3*7   +  1*4*7  + 1*7*1 = 143
 
-# Our two states are pop, or don't pop basically
-#
-# 
-
 
 class Solution:
     def maxCoins(self, nums: list[int]) -> int:
-        pass
+        nums = [1] +  nums + [1]
+        dp = {}
+
+        def dfs(l,r) -> int:
+            if l > r:
+                return 0
+
+            if (l,r) in dp:
+                return dp[(l,r)]
+
+            dp[(l,r)] = 0
+            for i in range(l,r + 1):
+                coins = nums[i] * nums[r+1] * nums[l-1]
+                coins += dfs(l,i-1) + dfs(i+1, r)
+                dp[(l,r)] = max(dp[l,r], coins)
+
+            return dp[(l,r)]
+
+        return dfs(1, len(nums) -2)
 
 def main():
     sol = Solution()
-    print(sol.maxCoins([4,2,3]))
+    print(sol.maxCoins([4,2,3,7]))
 
 if __name__ == "__main__":
     main()
