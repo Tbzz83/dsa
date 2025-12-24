50738c29db2e6cc7ad5642dd525491a16b5ccccc
tbzz83 <tbzz83@gmail.com>
2025-12-24 08:06:59 -0500

    Working push and print

--- linked_list/rs_ll/src/main.rs
+++ linked_list/rs_ll/src/main.rs
@@ -1,12 +1,23 @@
+use std::rc::Rc;
+
 use crate::node::Node;
 
 mod node;
 
+struct Data {
+    val: Option<Rc<i32>>,
+}
+
 fn main() {
+
     let mut head = Node::new(1);
 
     head.push(2);
     head.push(3);
+    head.push(4);
+    head.push(5);
+    //head.delete_ith(1);
+    head.print();
 
     //head.delete_ith(1);
 }
--- linked_list/rs_ll/src/node.rs
+++ linked_list/rs_ll/src/node.rs
@@ -1,7 +1,9 @@
+use std::{cell::RefCell, error::{}, fmt::Error, ops::DerefMut, rc::Rc};
+
 #[derive (Debug, Clone)]
 pub struct Node {
     pub value: i32,
-    pub next: Option<Box<Node>>,
+    pub next: Option<Rc<RefCell<Node>>>,
 }
 
 #[derive (Debug)]
@@ -15,28 +17,28 @@ impl std::fmt::Display for Node {
     }
 }
 
-impl <'a>Iterator for NodeIter<'a> {
-    type Item = &'a Node;
+//impl <'a>Iterator for NodeIter<'a> {
+//    type Item = &'a Node;
+//
+//    fn next(&mut self) -> Option<Self::Item> {
+//        if let Some(next) = &self.cur.next {
+//            // Advance iterator
+//            self.cur = next;
+//            return Some(next);
+//        }
+//
+//        None
+//    }
+//}
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = &self.cur.next {
-            // Advance iterator
-            self.cur = next;
-            return Some(next);
-        }
-
-        None
-    }
-}
-
-impl <'a> IntoIterator for &'a Node {
-    type Item = &'a Node;
-    type IntoIter = NodeIter<'a>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
-}
+//impl <'a> IntoIterator for &'a Node {
+//    type Item = &'a Node;
+//    type IntoIter = NodeIter<'a>;
+//
+//    fn into_iter(self) -> Self::IntoIter {
+//        self.iter()
+//    }
+//}
 
 //impl IntoIterator for Node {
 //    type Item = Node;
@@ -49,26 +51,9 @@ impl Node {
         Self { value, next: None }
     }
 
-//    pub fn delete_ith(&mut self, i: i32) {
-//        if i == 0 {
-//
-//        }
-//
-//        let mut j = 0;
-//
-//        let mut cur = &mut self.next;
-//
-//        while let Some(node) = cur {
-//            if j + 1 == i {
-//                println!("j: {j} CASE");
-//                if let Some(next) = &mut node.next {
-//                    node.next = next.next;
-//                }
-//            }
-//            cur = &mut node.next;
-//            j += 1;
-//        }
-//    }
+    pub fn delete_ith(&self, i: i32) {
+
+    }
 
     pub fn iter(&self) -> NodeIter<'_> {
         NodeIter {
@@ -77,25 +62,55 @@ impl Node {
     }
 
     pub fn push(&mut self, value: i32) {
-        let mut cur = &mut self.next;
-
-        while let Some(node) = cur {
-            cur = &mut node.next;
-        }
-
-        *cur = Some(Box::new(Node::new(value)));
+    // 1. Handle empty list
+    if self.next.is_none() {
+        self.next = Some(Rc::new(RefCell::new(Node { value, next: None })));
+        return;
     }
 
-    pub fn print(mut self) {
-        let mut print_str = format!("{}", self.value);
-        'outer: loop {
-            if let Some(next) = self.next {
-                self = *next;
-                print_str += &format!(" -> {}", self.value);
-            } else {
-                break 'outer;
+    // 2. Start at the first node
+    let mut cur = self.next.clone().unwrap();
+
+    // 3. Walk to the end
+    loop {
+        // We need a block or a temp variable to drop the borrow before we mutate
+        let next_node = cur.borrow().next.clone();
+
+        match next_node {
+            Some(next) => {
+                cur = next; // Move cursor forward
+            }
+            None => {
+                // We reached the end, insert and exit
+                cur.borrow_mut().next = Some(Rc::new(RefCell::new(Node { value, next: None })));
+                break; 
             }
         }
+    }
+}
+
+    pub fn print(&self) {
+        let mut print_str = format!("{}", self.value);
+
+        if self.next.is_none() {
+            println!("{print_str}");
+            return;
+        }
+
+        let mut cur = self.next.clone().unwrap();
+
+        loop {
+            print_str += format!(" -> {}", cur.borrow().value).as_str();
+            let next_node = cur.borrow().next.clone();
+
+            match next_node {
+                Some(next) => {
+                    cur = next
+                },
+                None => break,
+            }
+        }
+
         println!("{print_str}");
     }
 }
