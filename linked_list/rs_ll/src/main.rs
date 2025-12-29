62e8d7dcbfcd4272a723cb7d74fd249e254549b0
tbzz83 <tbzz83@gmail.com>
2025-12-28 17:14:52 -0800

    .

--- linked_list/rs_ll/src/main.rs
+++ linked_list/rs_ll/src/main.rs
@@ -1,201 +1,92 @@
-use std::rc::Rc;
+use std::ptr::{null, null_mut};
 
-// --- Data Structures ---
 
-pub struct List<T> {
-    head: Link<T>,
+#[derive (Debug)]
+struct LinkedList<T> 
+where T: std::fmt::Debug + std::fmt::Display,
+{
+    head: *mut Node<T>,
+    tail: *mut Node<T>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
-
-struct Node<T> {
-    elem: T,
-    next: Link<T>,
+#[derive (Debug)]
+struct Node<T>
+where T: std::fmt::Debug + std::fmt::Display,
+{
+    value: Option<T>,
+    next: *mut Node<T>,
+    prev: *mut Node<T>,
 }
 
-// --- Implementation ---
-
-impl<T> List<T> {
+impl <T>LinkedList<T>
+where T: std::fmt::Debug + std::fmt::Display,
+{
     pub fn new() -> Self {
-        List { head: None }
-    }
-
-    pub fn push(&mut self, elem: T) {
-        let new_node = Box::new(Node {
-            elem: elem,
-            next: self.head.take(),
-        });
-
-        self.head = Some(new_node);
-    }
-
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|node| {
-            self.head = node.next;
-            node.elem
-        })
-    }
-
-    pub fn peek(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.elem)
-    }
-
-    pub fn peek_mut(&mut self) -> Option<&mut T> {
-        self.head.as_mut().map(|node| &mut node.elem)
-    }
-
-    pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter(self)
-    }
-
-    pub fn iter(&self) -> Iter<'_, T> {
-        Iter {
-            next: self.head.as_deref(),
+        let empty: *mut Node<T> = null_mut();
+        LinkedList { 
+            head: empty, 
+            tail: empty,
         }
     }
 
-    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
-        IterMut {
-            next: self.head.as_deref_mut(),
+    // Push a new element to the front of the linked list. 
+    fn push(&mut self, val: T) {
+        let new_node = Box::into_raw(Box::new(Node {
+            value: Some(val),
+            next: self.head,
+            prev: null_mut(),
+        }));
+
+        unsafe {
+            if !self.head.is_null() {
+                (*self.head).prev = new_node;
+            } 
+        }
+
+        self.head = new_node;
+    }
+
+    // Pops from the right
+    fn pop(&mut self) -> Box<Node<T>> {
+        unsafe {
+            let prev = (*self.tail).prev;
+            let popped = Box::from_raw(self.tail);
+            self.tail = prev;
+            popped
         }
     }
-}
 
-// Manual Drop to prevent stack overflow on long lists
-impl<T> Drop for List<T> {
-    fn drop(&mut self) {
-        let mut cur_link = self.head.take();
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = boxed_node.next.take();
+    fn print(&self) {
+        let mut cur: *const Node<T> = self.head;
+        while !cur.is_null() {
+            unsafe {
+                println!("{:?}", *cur);
+                cur = (*cur).next;
+            }
         }
     }
 }
 
-// --- Iterators ---
-
-pub struct IntoIter<T>(List<T>);
-
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop()
+impl <T>Node<T>
+where T: std::fmt::Debug + std::fmt::Display,
+{
+    pub fn new() -> Self {
+        Node { 
+            value: None,
+            next: null_mut(),
+            prev: null_mut() 
+        }
     }
 }
-
-pub struct Iter<'a, T> {
-    next: Option<&'a Node<T>>,
-}
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.map(|node| {
-            self.next = node.next.as_deref();
-            &node.elem
-        })
-    }
-}
-
-pub struct IterMut<'a, T> {
-    next: Option<&'a mut Node<T>>,
-}
-
-impl<'a, T> Iterator for IterMut<'a, T> {
-    type Item = &'a mut T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.take().map(|node| {
-            self.next = node.next.as_deref_mut();
-            &mut node.elem
-        })
-    }
-}
-
-// --- Entry Point ---
 
 fn main() {
-    let mut list = List::new();
-    list.push(1);
-    list.push(2);
-    println!("Popped: {:?}", list.pop());
-}
+    let mut new_list = LinkedList::<i32>::new();
 
-// --- Tests ---
-
-#[cfg(test)]
-mod test {
-    use super::*; // Now correctly pulls from the crate root
-
-    #[test]
-    fn basics() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
-
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
-
-        list.push(4);
-        list.push(5);
-
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
-    }
-
-    #[test]
-    fn peek() {
-        let mut list = List::new();
-        assert_eq!(list.peek(), None);
-        assert_eq!(list.peek_mut(), None);
-        list.push(1); list.push(2); list.push(3);
-
-        assert_eq!(list.peek(), Some(&3));
-        assert_eq!(list.peek_mut(), Some(&mut 3));
-
-        list.peek_mut().map(|value| {
-            *value = 42
-        });
-
-        assert_eq!(list.peek(), Some(&42));
-        assert_eq!(list.pop(), Some(42));
-    }
-
-    #[test]
-    fn into_iter() {
-        let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
-
-        let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(1));
-        assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn iter() {
-        let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
-
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&1));
-    }
-
-    #[test]
-    fn iter_mut() {
-        let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
-
-        let mut iter = list.iter_mut();
-        assert_eq!(iter.next(), Some(&mut 3));
-        assert_eq!(iter.next(), Some(&mut 2));
-        assert_eq!(iter.next(), Some(&mut 1));
-    }
+    new_list.push(1);
+    new_list.push(2);
+    new_list.push(3);
+    new_list.push(4);
+    new_list.print();
+    let popped = new_list.pop();
+    new_list.print();
 }
