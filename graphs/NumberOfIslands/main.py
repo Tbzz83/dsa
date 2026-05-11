84078fae197772c2db7b0dfc3211664aa0373601
tbzz83 <tbzz83@gmail.com>
2026-05-11 10:34:52 -0400

    CloneGraph pretty tough

--- graphs/NumberOfIslands/main.py
+++ graphs/NumberOfIslands/main.py
@@ -1,83 +1,68 @@
-# Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
-#
-# An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water).
-# Traverse through all coords on our grid. If we encounter land ("1"), then we can traverse starting from that point,
-# and find all regions of land that come into direct contact with it.
-#
-# We will use a seen set, to keep track of coords that we have visited set(tuple[int])
-#
-# Only traverse if the current coord we are on is not in the seen set
-#
-# Ignore "0"
-#
-# Once a "1" is found, increment islands++ so long as that coord is not in the seen set
-#
-# Assume our grid is square
-from collections import deque
+'''
+Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
+
+An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water). 
+
+'''
+
+import collections
 
 
-def numIslands(grid: list[list[str]]) -> int:
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        visited = set()
+        res = 0
+        NUM_ROWS = len(grid)
+        NUM_COLS = len(grid[0])
 
-    islands: int = 0
-    seen: set[tuple[int, ...]] = set()
-    ROWS, COLS = len(grid), len(grid[0])
 
-    # Point of this function is to modify seen
-    def traverse(coord: tuple[int, ...]):
-        q: deque[tuple[int, ...]] = deque()
-        q.append(coord)
-        options: list[list[int]] = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        def bfs(r: int,c: int):
+            q = collections.deque()
+            q.appendleft((r,c))
 
-        while q:
-            # Unpack
-            cur_r, cur_c = q.popleft()
+            while len(q) > 0:
+                coord: tuple[int,int] = q.popleft()
+                cur_r, cur_c = coord[0], coord[1]
+                opts = [(cur_r,cur_c-1),
+                        (cur_r,cur_c+1),
+                        (cur_r-1,cur_c),
+                        (cur_r+1,cur_c)]
 
-            for opt in options:
-                new_r, new_c = cur_r + opt[0], opt[1] + cur_c
-                new_coord = (new_r, new_c)
+                for opt in opts:
+                    opt_r, opt_c = opt[0], opt[1]
+                    if opt_r in range(NUM_ROWS) and opt_c in range(NUM_COLS) and grid[opt_r][opt_c] == "1" and (opt_r,opt_c) not in visited:
+                        visited.add((opt_r,opt_c))
+                        q.appendleft((opt_r,opt_c))
 
-                if (new_r in range(ROWS) and
-                    new_c in range(COLS) and
-                    (new_r, new_c) not in seen and
-                        grid[new_r][new_c] == "1"):
-                    q.append(new_coord)
-                    seen.add(new_coord)
 
-    for r in range(ROWS):
-        for c in range(COLS):
-            value: str = grid[r][c]
-            if value == "0":
-                continue
+        for r in range(NUM_ROWS):
+            for c in range(NUM_COLS):
+                val = grid[r][c]
 
-            coord: tuple[int, ...] = (r, c)
-
-            if coord not in seen:
-                seen.add(coord)
-                traverse(coord)
-                islands += 1
-
-    return islands
-
+                if val == "1" and (r,c) not in visited:
+                    visited.add((r,c))
+                    res += 1
+                    bfs(r,c)
+                else:
+                    continue
+        return res
 
 def main():
     grid = [
-        ["0", "1", "1", "1", "0"],
-        ["0", "1", "0", "1", "0"],
-        ["1", "1", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"]
-    ]
-
-    print(numIslands(grid))
-    print("---")
-
+    ["0","1","1","1","0"],
+    ["0","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]]
     grid = [
-        ["1", "1", "0", "0", "1"],
-        ["1", "1", "0", "0", "1"],
-        ["0", "0", "1", "0", "0"],
-        ["0", "0", "0", "1", "1"]
-    ]
+    ["1","1","0","0","1"],
+    ["1","1","0","0","1"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+  ]
 
-    print(numIslands(grid))
+    sol = Solution()
+    print(sol.numIslands(grid))
+
 
 
 if __name__ == "__main__":
